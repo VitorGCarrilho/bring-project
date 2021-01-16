c41ae7e096d27ed046d6bfc5be8c8ce207824322
Vitor Carrilho <vitorg.carrilho@gmail.com>
2021-01-16 17:17:18 -0300

    adding jacoco code coverage

---
+++ src/test/java/com/carrilho/vitor/service/TransactionServiceTest.java
@@ -0,0 +1,14 @@
+package com.carrilho.vitor.service;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TransactionServiceTest {
+	
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
