f8cf15e3d21696969536d36f33b6bf0f30b523e7
Vitor Carrilho <vitorg.carrilho@gmail.com>
2021-01-16 16:50:02 -0300

    corrigindo o path variable

--- src/main/java/com/carrilho/vitor/controller/TransactionController.java
+++ src/main/java/com/carrilho/vitor/controller/TransactionController.java
@@ -6,12 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carrilho.vitor.client.response.TransactionResponse;
-import com.carrilho.vitor.dto.TransactionsDTO;
 import com.carrilho.vitor.service.TransactionService;
 
 @RestController
@@ -27,7 +27,7 @@ public class TransactionController {
 	}
 	
 	@GetMapping("/{accountId}")
-	public ResponseEntity<List<TransactionResponse>> get(@RequestParam String accountId) {
+	public ResponseEntity<List<TransactionResponse>> get(@PathVariable String accountId) {
 		logger.info("receiving hello get");
 		List<TransactionResponse> transactionList = transactionService.getByAccountId(accountId);		
 		return ResponseEntity.ok(transactionList);
