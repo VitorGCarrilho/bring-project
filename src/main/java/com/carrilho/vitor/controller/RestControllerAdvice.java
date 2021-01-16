16ab9b1efda0411a0ba163fba389196f9cd7b62e
Vitor Carrilho <vitorg.carrilho@gmail.com>
2021-01-15 22:42:59 -0300

    Ajustando configs do swagger em xml

--- src/main/java/com/carrilho/vitor/controller/RestControllerAdvice.java
+++ src/main/java/com/carrilho/vitor/controller/RestControllerAdvice.java
@@ -8,15 +8,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.carrilho.vitor.exception.AccountNotFoundException;
-
 @ControllerAdvice
 public class RestControllerAdvice extends ResponseEntityExceptionHandler  {
 
-	@ExceptionHandler(value = { AccountNotFoundException.class })
-	public ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
-		String bodyOfResponse = "This should be application specific";
-		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
-	}
-
+	
 }
